25e97a2f049e8a7cdbf648f89cd5379223d087eb
iberis2 <snow9402@gmail.com>
2023-04-18 07:25:10 +0900

    feat: ask 페이지 하드코딩, uiw 웹 에디터 설치(#35)

--- client/src/pages/Ask.jsx
+++ client/src/pages/Ask.jsx
@@ -1,3 +1,34 @@
+import { useState } from 'react'
+import MDEditor from '@uiw/react-md-editor'
+
 export default function Ask() {
-  return <>Ask</>
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+
+  const onSubmit = e => {
+    e.preventDefault()
+  }
+
+  return (
+    <>
+      <h2>Ask a public question</h2>
+      <form onSubmit={onSubmit}>
+        <div>
+          <h3>Title</h3>
+          <p>{`Be specific and imagine you're asking a question to another person`}</p>
+          <input type='text' className='title-input' value={title} onChange={setTitle} />
+        </div>
+        <div>
+          <h3>Body</h3>
+          <p>
+            The body of your question contains your problem details and results. Minimum 30
+            characters.
+          </p>
+          <MDEditor value={content} onChange={setContent} preview='edit' />
+          {/* <MDEditor.Markdown style={{ padding: 10 }} source={content} /> */}
+        </div>
+        <button type='submit'>Post your question</button>
+      </form>
+    </>
+  )
 }
