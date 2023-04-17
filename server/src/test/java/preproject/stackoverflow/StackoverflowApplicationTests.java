b746d481d2594b894a16b998f930cc224cf4ed98
kim-ksp7331 <ksp7331@gmail.com>
2023-04-17 14:18:41 +0900

    Chore : 스프링 개발환경 설정

---
+++ server/src/test/java/preproject/stackoverflow/StackoverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package preproject.stackoverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackoverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
