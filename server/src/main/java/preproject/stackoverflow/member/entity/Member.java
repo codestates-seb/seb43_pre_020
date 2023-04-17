c1bbf010bb0b895cd98399e9e54baaeea74fe3af
kim-ksp7331 <ksp7331@gmail.com>
2023-04-17 16:45:52 +0900

    feat : Member 엔티티 클래스 구현

--- server/src/main/java/preproject/stackoverflow/member/entity/Member.java
+++ server/src/main/java/preproject/stackoverflow/member/entity/Member.java
@@ -8,6 +8,7 @@ import preproject.stackoverflow.comment.entity.Comment;
 import preproject.stackoverflow.question.entity.Question;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,10 +20,51 @@ public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long memberId;
+    @Column(length = 100)
+    private String password;
+    @Column(length = 100)
+    private String email;
+    @Column(length = 100)
+    private String displayName;
+    @Column(length = 500)
+    private String aboutMe;
+    @Column(length = 100)
+    private String title;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private MemberStatus memberStatus = MemberStatus.MEMBER_ACTIVE;
+    @Column(nullable = false)
+    private LocalDateTime joinTime = LocalDateTime.now();
+    @Column(nullable = false)
+    private LocalDateTime lastJoinTime = LocalDateTime.now();
+    @Column(nullable = false)
+    private LocalDateTime lastActivityTime = LocalDateTime.now();
     @OneToMany(mappedBy = "member")
     private List<Question> questions = new ArrayList<>();
     @OneToMany(mappedBy = "member")
     private List<Answer> answers = new ArrayList<>();
     @OneToMany(mappedBy = "member")
     private List<Comment> comments = new ArrayList<>();
+
+    public void addQuestion(Question question) {
+        questions.add(question);
+    }
+
+    public void addAnswer(Answer answer) {
+        answers.add(answer);
+    }
+
+    public void addComment(Comment comment) {
+        comments.add(comment);
+    }
+
+    public enum MemberStatus {
+        MEMBER_ACTIVE("활동중"),
+        MEMBER_QUIT("탈퇴 상태");
+        @Getter
+        private String status;
+        MemberStatus(String status) {
+            this.status = status;
+        }
+    }
 }
