b4819ddab6841572145eb6930bc8b41ae6dcbc1b
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-04-17 20:26:21 +0900

    feat: redux 초기 세팅(#33)

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 )
