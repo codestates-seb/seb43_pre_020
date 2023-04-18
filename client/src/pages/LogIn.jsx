bd5c1926425c6c2a21842d38c2dc266e3e9c4c4b
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-04-18 20:01:48 +0900

    design: 로고, oauth ui 구현

--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -1,3 +1,42 @@
-export default function LogIn() {
-  return <>LogIn</>
+import styles from './login.module.scss'
+
+function LogIn() {
+  return (
+    <div className={styles.container}>
+      <a href='/'>
+        <img
+          className={styles.logoImg}
+          src={`${process.env.PUBLIC_URL}/assets/logo-only-graphic.svg`}
+          alt='stackoverflow logo img'
+        />
+      </a>
+      <div className={styles.Oauths}>
+        <button className={styles.googleBtn} type='button'>
+          <img src={`${process.env.PUBLIC_URL}/assets/icons/google.svg`} alt='google logo img' />
+          Log in with Google
+        </button>
+        <button className={styles.githubBtn} type='button'>
+          <img src={`${process.env.PUBLIC_URL}/assets/icons/github.svg`} alt='github logo img' />
+          Log in with GitHub
+        </button>
+      </div>
+      <form className={styles.form}>
+        <label className={styles.label}>
+          Email
+          <input className={styles.input} type='email' />
+        </label>
+        <label className={styles.label}>
+          Password
+          <input className={styles.input} type='password' />
+        </label>
+        <button className={styles.button} type='button'>
+          Log in
+        </button>
+      </form>
+      <span>Don’t have an account?</span>
+      <a href='/signup'>Sign up</a>
+    </div>
+  )
 }
+
+export default LogIn
