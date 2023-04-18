7d43c2346a63f7fbb2b23f5652fc4ac2355a6810
iberis2 <snow9402@gmail.com>
2023-04-18 11:34:55 +0900

    feat: ask 페이지 구현(#35)
    data에 title과 content 담아서 submit 시 preview 보여줌
    scss 적용
    label과 input 연결에러 관련  eslint 설정 수정

--- client/src/pages/Ask.jsx
+++ client/src/pages/Ask.jsx
@@ -1,34 +1,93 @@
 import { useState } from 'react'
 import MDEditor from '@uiw/react-md-editor'
+import styles from './Ask.module.scss'
 
 export default function Ask() {
-  const [title, setTitle] = useState('')
-  const [content, setContent] = useState('')
+  const [data, setData] = useState({
+    title: '',
+    content: '',
+  })
 
-  const onSubmit = e => {
+  const [isSubmit, setIsSubmit] = useState(true)
+
+  const onChange = event => {
+    if (event.target) {
+      setData(prev => {
+        return { ...prev, title: event.target.value }
+      })
+    } else {
+      setData(prev => {
+        return { ...prev, content: event }
+      })
+    }
+  }
+
+  const onPreviewSubmit = e => {
     e.preventDefault()
+    setIsSubmit(!isSubmit)
+  }
+
+  const onAmendClick = () => {
+    setIsSubmit(!isSubmit)
   }
 
   return (
-    <>
-      <h2>Ask a public question</h2>
-      <form onSubmit={onSubmit}>
-        <div>
-          <h3>Title</h3>
-          <p>{`Be specific and imagine you're asking a question to another person`}</p>
-          <input type='text' className='title-input' value={title} onChange={setTitle} />
-        </div>
-        <div>
-          <h3>Body</h3>
-          <p>
-            The body of your question contains your problem details and results. Minimum 30
-            characters.
-          </p>
-          <MDEditor value={content} onChange={setContent} preview='edit' />
-          {/* <MDEditor.Markdown style={{ padding: 10 }} source={content} /> */}
-        </div>
-        <button type='submit'>Post your question</button>
-      </form>
-    </>
+    <div className={styles.ask}>
+      <h2 className={styles.headline}>Ask a public question</h2>
+      {isSubmit ? (
+        <form onSubmit={onPreviewSubmit}>
+          <div className={styles.titleContainer}>
+            <label htmlFor='title' className={styles.title}>
+              Title
+            </label>
+            <p>{`Be specific and imagine you're asking a question to another person.`}</p>
+            <input
+              id='title'
+              type='text'
+              className={styles.titleInput}
+              value={data.title}
+              onChange={onChange}
+              placeholder='제목을 입력하세요'
+              required
+            />
+          </div>
+          <div className={styles.bodyContainer}>
+            <label htmlFor='body' className={styles.body}>
+              Body
+            </label>
+            <p>
+              The body of your question contains your problem details and results. Minimum 30
+              characters.
+            </p>
+            <MDEditor id='body' value={data.content} onChange={onChange} preview='edit' required />
+          </div>
+          <button className={styles.btn} type='submit'>
+            Post your question
+          </button>
+        </form>
+      ) : (
+        <form className={styles.preview}>
+          <div className={styles.titleContainer}>
+            <h2 className={styles.title}>Title</h2>
+            <p>{`Be specific and imagine you're asking a question to another person`}</p>
+            <div className={styles.titlePreview}>{data.title}</div>
+          </div>
+          <div className={styles.bodyContainer}>
+            <h2 className={styles.body}>Body</h2>
+            <p>
+              The body of your question contains your problem details and results. Minimum 30
+              characters.
+            </p>
+            <MDEditor.Markdown className={styles.bodyPreview} source={data.content} />
+          </div>
+          <button className={styles.btn} type='submit'>
+            Post your question
+          </button>
+          <button className={styles.btnAmend} type='button' onClick={onAmendClick}>
+            Amend draft
+          </button>
+        </form>
+      )}
+    </div>
   )
 }
