6387753dfd27811f74e91cf0f386af83e17023d0
snow <120469477+iberis2@users.noreply.github.com>
2023-04-19 11:37:45 +0900

    feat: mypage 프로필 수정 틀 구현 (#42)
    * feat: 마이페이지 프로필 수정 input 구현
    scss 적용(#40)
    
    * feat: name, title, about me 하드코딩으로 변경(#40)
    
    * refactor: 반복되는 input 컴포넌츠화(#40)

--- client/src/pages/MyPage.jsx
+++ client/src/pages/MyPage.jsx
@@ -1,3 +1,44 @@
+import { useState } from 'react'
+import styles from './MyPage.module.scss'
+import Profile from '../component/Profile'
+
 export default function MyPage() {
-  return <>MyPage</>
+  const [data, setData] = useState({
+    name: 'Initial',
+    title: 'Title',
+    content: 'About me',
+  })
+
+  const onChange = e => {
+    const { id, value } = e.target
+    setData(prev => {
+      return { ...prev, [id]: value }
+    })
+  }
+
+  const onSubmit = e => {
+    e.preventDefault()
+  }
+
+  const list = ['name', 'title', 'content']
+
+  return (
+    <div className={styles.myPageContainer}>
+      <div>
+        <h1 className={styles.headName}>Name</h1>
+        <h3 className={styles.headTitle}>Title</h3>
+        <p className={styles.headContent}>About me</p>
+      </div>
+      <form onSubmit={onSubmit}>
+        <div className={styles.inputBox}>
+          {list.map(element => (
+            <Profile key={element} type={element} data={data[element]} onChange={onChange} />
+          ))}
+        </div>
+        <button type='submit' className={styles.btn}>
+          Save profile
+        </button>
+      </form>
+    </div>
+  )
 }
