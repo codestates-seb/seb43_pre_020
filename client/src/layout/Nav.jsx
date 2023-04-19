7fc18d96acc4c21873742b6d6cb2f90d2f8d1bdc
Sangbeom Heo <mansaout@gmail.com>
2023-04-19 17:40:56 +0900

    feat: Nav 구현 (#36)
    pathname 별로 스타일링 분기 처리

--- client/src/layout/Nav.jsx
+++ client/src/layout/Nav.jsx
@@ -1,14 +1,35 @@
+import { Link, useLocation } from 'react-router-dom'
 import styles from './Nav.module.scss'
 
 export default function Nav() {
+  const { pathname } = useLocation()
+
+  const linkMap = [
+    { id: 1, label: 'Questions', path: '/' },
+    { id: 2, label: 'Tags', path: '/tags' },
+    { id: 3, label: 'Users', path: '/mypage' },
+    { id: 4, label: 'Companies', path: '/companies' },
+  ]
+
+  const isActive = path => pathname === path
+
+  const isQuestion = label =>
+    label === 'Questions' && (pathname === '/' || pathname.includes('/questions'))
+
   return (
     <nav className={styles.nav}>
-      <ul>
-        <li>Questions</li>
-        <li>Tags</li>
-        <li>Users</li>
-        <li>Companies</li>
-      </ul>
+      {linkMap.map(link => (
+        <Link
+          key={link.id}
+          to={link.path}
+          className={isActive(link.path) ? styles.itemActive : styles.item}
+        >
+          {isQuestion(link.label) && (
+            <img src={`${process.env.PUBLIC_URL}/assets/icons/globe.svg`} alt='Questions' />
+          )}
+          {link.label}
+        </Link>
+      ))}
     </nav>
   )
 }
