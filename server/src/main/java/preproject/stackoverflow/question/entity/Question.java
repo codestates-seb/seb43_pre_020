85ed82f835f74bc7533e1f1ec767573464573bd3
kim-ksp7331 <119999208+kim-ksp7331@users.noreply.github.com>
2023-04-20 12:52:05 +0900

    Revert "feat/be/질문등록기능 완료 (#63)"
    This reverts commit 7e7e1fda067a4237d8fe4e6a4cd43664f6a56c22.

--- server/src/main/java/preproject/stackoverflow/question/entity/Question.java
+++ server/src/main/java/preproject/stackoverflow/question/entity/Question.java
@@ -5,12 +5,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import preproject.stackoverflow.answer.entity.Answer;
 import preproject.stackoverflow.comment.entity.Comment;
-import preproject.stackoverflow.entity.BaseEntity;
 import preproject.stackoverflow.member.entity.Member;
 
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,51 +16,16 @@ import java.util.List;
 @NoArgsConstructor
 @Getter
 @Setter
+public class Question {
 
-public class Question{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long questionId;
-    @Column(nullable = false, length = 100)
-    private String title;
-    @Column(nullable = false)
-    private String content;
-    @Column(nullable = false)
-    private LocalDateTime createTime = LocalDateTime.now();
-
-    @Column(nullable = false)
-    private Integer view = 0;
-
-    // 질문 상태 : 등록, 답변 완료, 삭제
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false, length = 30)
-    private QuestionStatus questionStatus = QuestionStatus.QUESTION_REGISTRATION;
-
-    
-    // 질문 1 : 답변 다
-    @OneToMany(mappedBy = "question", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
-    private List<Answer> answer;
-
-    // 외래키 매핑 = 멤버 1 : 질문 다
     @ManyToOne
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
-
     @OneToMany(mappedBy = "question", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
     private List<Answer> answers = new ArrayList<>();
     @OneToMany(mappedBy = "question", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
     private List<Comment> comments = new ArrayList<>();
-
-    public enum QuestionStatus {
-        QUESTION_REGISTRATION("질문 등록"),
-        QUESTION_ANSWERED("답변 완료"),
-        QUESTION_DELETE("질문 삭제");
-        @Getter
-        private final String status;
-
-        QuestionStatus(String status) {
-            this.status = status;
-        }
-    }
-    
 }
