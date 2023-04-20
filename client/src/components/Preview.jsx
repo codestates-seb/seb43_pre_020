b914dc19b4c7278779f91d25915587fd7a902c8d
iberis2 <snow9402@gmail.com>
2023-04-20 16:40:36 +0900

    refactor: preview의 title을 하위 컴포넌츠로 뺌(#70)

--- client/src/components/Preview.jsx
+++ client/src/components/Preview.jsx
@@ -1,34 +1,42 @@
 import MDEditor from '@uiw/react-md-editor'
 import styles from '../pages/Ask.module.scss'
 
+const BODYTEXT =
+  'The body of your question contains your problem details and results. Minimum 30 characters.'
+
 function Preview({ data, writeDone, setWriteDone }) {
   const onAmendClick = () => {
     setWriteDone(!writeDone)
   }
 
   return (
-    <form className={styles.preview}>
-      <div className={styles.titleContainer}>
-        <h2 className={styles.title}>Title</h2>
-        <p>{`Be specific and imagine you're asking a question to another person`}</p>
-        <div className={styles.titlePreview}>{data.title}</div>
-      </div>
-      <div className={styles.bodyContainer}>
-        <h2 className={styles.body}>Body</h2>
-        <p>
-          The body of your question contains your problem details and results. Minimum 30
-          characters.
-        </p>
-        <MDEditor.Markdown className={styles.bodyPreview} source={data.content} />
-      </div>
-      <button className={styles.btn} type='submit'>
-        Post your question
-      </button>
-      <button className={styles.btnAmend} type='button' onClick={onAmendClick}>
-        Amend draft
-      </button>
-    </form>
+    <div>
+      <TitleForm title={data.title} />
+      <form className={styles.preview}>
+        <div className={styles.bodyContainer}>
+          <h2 className={styles.body}>Body</h2>
+          <p>{BODYTEXT}</p>
+          <MDEditor.Markdown className={styles.bodyPreview} source={data.content} />
+        </div>
+        <button className={styles.btn} type='submit'>
+          Post your question
+        </button>
+        <button className={styles.btnAmend} type='button' onClick={onAmendClick}>
+          Amend draft
+        </button>
+      </form>
+    </div>
   )
 }
 
 export default Preview
+
+function TitleForm({ title }) {
+  return (
+    <div className={styles.titleContainer}>
+      <h2 className={styles.title}>Title</h2>
+      <p>{`Be specific and imagine you're asking a question to another person`}</p>
+      <div className={styles.titlePreview}>{title}</div>
+    </div>
+  )
+}
