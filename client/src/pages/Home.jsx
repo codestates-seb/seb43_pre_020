8296b40a580e6ceb624ce8c597fc0a9d4d243d56
snow <120469477+iberis2@users.noreply.github.com>
2023-04-20 14:19:02 +0900

    Feat: home UI 구현 (#61)
    * style: 컴포넌츠폴더s붙임, &:hover로 바꿈(#53)
    
    * style: 컴포넌츠폴더s붙임(#53)
    
    * style: jsx로 확장자 변경
    
    * feat: home 화면 틀 구현 완료
    
    * feat: ask 웹 에디터 무조건 light 모드로 고정
    
    * feat: json 더미 데이터 생성, json-server 설치(#53)
    
    * feat: json 서버에서 데이터 받아오도록 변경
    
    * feat: 답변 채택 여부에 따라 초록색 표시
    
    * feat: 질문자 이름 파란색으로 고침
    
    * feat: 반응형 웹으로 구현(#53)
    
    가로 950px 이하에서 votes와 answer 가 질문 위로 올라감

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,51 @@
+import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import styles from './Home.module.scss'
+import Questions from '../components/Questions'
+import axios from '../api/instance'
+
 export default function Home() {
-  return <>Home</>
+  const [datas, setData] = useState([])
+
+  const getData = async ({ page, size }) => {
+    const query = `page=${page}&size=${size}`
+    try {
+      const { data } = await axios.get(`/questions?${query}`)
+      setData(data)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  useEffect(() => {
+    getData({ page: 0, size: 6 })
+  }, [])
+
+  return (
+    <div className={styles.homeContainer}>
+      <HomeHeader datas={datas} />
+      {datas.map(d => {
+        return (
+          <div key={d.id}>
+            <div className={styles.line} />
+            <Questions data={d} />
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+function HomeHeader({ datas }) {
+  return (
+    <div>
+      <div className={styles.homeHeader}>
+        <h2>All Questions</h2>
+        <Link to='/ask' className={styles.btn}>
+          Ask Question
+        </Link>
+      </div>
+      <h3>{datas.length} questions</h3>
+    </div>
+  )
 }
