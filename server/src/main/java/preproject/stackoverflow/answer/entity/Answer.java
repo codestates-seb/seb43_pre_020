f6d15f8d1037d6033f4e15c3d6a1d23741418376
sksjw95 <tjwldnd95@hanmail.net>
2023-04-20 11:11:27 +0900

    feat: 답변 등록 기능

--- server/src/main/java/preproject/stackoverflow/answer/entity/Answer.java
+++ server/src/main/java/preproject/stackoverflow/answer/entity/Answer.java
@@ -8,16 +8,25 @@ import preproject.stackoverflow.member.entity.Member;
 import preproject.stackoverflow.question.entity.Question;
 
 import javax.persistence.*;
+import java.math.BigInteger;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import static com.fasterxml.jackson.databind.type.LogicalType.DateTime;
 
-/*@Entity
+@Entity
 @NoArgsConstructor
 @Getter
 @Setter
 public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long answerId;
+
+    @Column(name = "BODY", nullable = false)
+    private String body;
+
     @ManyToOne
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
@@ -26,105 +35,27 @@ public class Answer {
     private Question question;
     @OneToMany(mappedBy = "answer")
     private List<Comment> comments = new ArrayList<>();
-} */
 
-@Entity
-@NoArgsConstructor
-@Getter
-@Setter
-public class Answer {
+    @Column(name = "CREATION_DATE", nullable = false)
+    private LocalDateTime creationdate = LocalDateTime.now();
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "MEMBER_ID")
-    private Member member;
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "ANSWER_STATUS" , length = 20, nullable = false)
+    private AnswerStatus answerStatus = AnswerStatus.ANSWER_REGISTRATION;
+    public  enum AnswerStatus{
+        ANSWER_REGISTRATION( "답변 등록"),
+        ANSWER_CHOICE( "답변 채택");
 
-    @ManyToOne
-    @JoinColumn(name = "QUESTION_ID")
-    private Question question;
 
-    @OneToMany(mappedBy = "answer", cascade = CascadeType.ALL)
-    private List<Comment> comments = new ArrayList<>();
-    @JoinColumn(name = "BODY")
-    private String body;
-    private   DateTime createdDate;
+        @Getter
+        private String status;
 
-    public Answer(Member member, Question question, String body) {
-        this.member = member;
-        this.question = question;
-        this.text = text;
-        this.createdDate = DateTime.now();
-    }
-
-    public void addComment(Comment comment) {
-        comments.add(comment);
-        comment.setAnswer(this);
-    }
-
-    public void removeComment(Comment comment) {
-        comments.remove(comment);
-        comment.setAnswer(null);
-    }
-
-    // CRUD functions
-
-    public static Answer createAnswer(Member member, Question question, String body) {
-        Answer answer = new Answer(member, question, body);
-        EntityManager entityManager = JPAUtil.getEntityManager();
-        EntityTransaction transaction = entityManager.getTransaction();
-        try {
-            transaction.begin();
-            entityManager.persist(answer);
-            transaction.commit();
-        } catch (Exception ex) {
-            if (transaction != null && transaction.isActive()) {
-                transaction.rollback();
-            }
-            ex.printStackTrace();
-        } finally {
-            entityManager.close();
+        AnswerStatus(String status){
+            this.status =status;
         }
-        return answer;
-    }
 
 
-
-    public static void updateAnswer(Answer answer) {
-        EntityManager entityManager = JPAUtil.getEntityManager();
-        EntityTransaction transaction = entityManager.getTransaction();
-        try {
-            transaction.begin();
-            entityManager.merge(answer);
-            transaction.commit();
-        } catch (Exception ex) {
-            if (transaction != null && transaction.isActive()) {
-                transaction.rollback();
-            }
-            ex.printStackTrace();
-        } finally {
-            entityManager.close();
-        }
-    }
-
-    public static void deleteAnswer(Answer answer) {
-        EntityManager entityManager = JPAUtil.getEntityManager();
-        EntityTransaction transaction = entityManager.getTransaction();
-        try {
-            transaction.begin();
-            entityManager.remove(answer);
-            transaction.commit();
-        } catch (Exception ex) {
-            if (transaction != null && transaction.isActive()) {
-                transaction.rollback();
-            }
-            ex.printStackTrace();
-        } finally {
-            entityManager.close();
-        }
     }
 }
-//  Member, Question 및 Comment 엔터티를 이미 만들고 데이터베이스 연결을 관리하기 위해
-//  JPAUtil 클래스를 설정했다고 가정합니다.
+
