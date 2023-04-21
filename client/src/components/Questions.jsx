3c251e20a371567d965f52851e25893ad6a18b39
iberis2 <snow9402@gmail.com>
2023-04-21 11:53:06 +0900

    feat: 작성 날짜 표시 ~분 전으로 표시 함수로 작성

--- client/src/components/Questions.jsx
+++ client/src/components/Questions.jsx
@@ -1,12 +1,41 @@
 import { Link, useParams } from 'react-router-dom'
 import styles from '../pages/Home.module.scss'
 
+function calDate(date) {
+  const created = new Date(date)
+  const difference = new Date() - created
+  if (difference < 1000 * 60) {
+    return 'now'
+  }
+  if (difference < 1000 * 60 * 60) {
+    const minute = parseInt(difference / (1000 * 60), 10)
+    return `${minute}분 전`
+  }
+  if (difference < 1000 * 60 * 60 * 24) {
+    const hour = parseInt(difference / (1000 * 60 * 60), 10)
+    return `${hour}시간 전`
+  }
+  if (difference < 1000 * 60 * 60 * 24 * 7) {
+    const day = parseInt(difference / (1000 * 60 * 60 * 24), 10)
+    return `${day}일 전`
+  }
+  if (difference < 1000 * 60 * 60 * 24 * 7 * 4) {
+    const week = parseInt(difference / (1000 * 60 * 60 * 24 * 7), 10)
+    return `${week}주 전`
+  }
+  if (difference < 1000 * 60 * 60 * 24 * 7 * 4 * 12) {
+    const month = parseInt(difference / (1000 * 60 * 60 * 24 * 7 * 4), 10)
+    return `${month}개월 전`
+  }
+  const year = created.getFullYear()
+  const month = created.getMonth() + 1
+  const dates = created.getDate()
+  return `${year}년 ${month}월 ${dates}일`
+}
+
 function Questions({ data }) {
   const { id } = useParams()
-  const date = new Date(data.date)
-  const YEAR = date.getFullYear()
-  const MONTH = date.getMonth() + 1
-  const DATE = date.getDate()
+  const date = calDate(data.date)
 
   return (
     <div className={styles.questionContainer}>
@@ -22,7 +51,7 @@ function Questions({ data }) {
           <div className={styles.content}>{data.content}</div>
           <div className={styles.nameAndDate}>
             <span className={styles.questioner}>{data.questioner}</span>
-            <span className={styles.date}>{`${YEAR}-${MONTH}-${DATE}`}</span>
+            <span className={styles.date}>{date}</span>
           </div>
         </div>
       </div>
