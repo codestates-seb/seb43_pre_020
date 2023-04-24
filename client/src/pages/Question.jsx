d26d1608d815ffffbb37dd18f17bcde6203d7591
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-04-24 12:55:24 +0900

    feat: 답변 채택 추가(#81)

--- client/src/pages/Question.jsx
+++ client/src/pages/Question.jsx
@@ -22,15 +22,22 @@ function Question() {
         answerId: 1,
         body: '답변이당',
         memberId: 1,
-        answerer: 'kim',
+        answerer: 'Seo',
         date: '2023-04-19T17:07:44.614834',
         comments: [
           {
             commentId: 1,
-            body: '답변의 댓글이당',
+            body: '답변의 댓글이당 댓글이이러러러러러러러러러러러러러러러럴러ㅓ러럴러러러러러러러러러럴게 길다아아아아아아아아아아아아앙',
+            memberId: 1,
+            commenter: 'Park',
+            date: '2023-04-18T17:07:51.69199',
+          },
+          {
+            commentId: 2,
+            body: '답변의 댓글이당2',
             memberId: 1,
             commenter: 'kim',
-            date: '2023-04-20T17:07:51.69199',
+            date: '2023-04-17T17:07:51.69199',
           },
         ],
       },
@@ -39,14 +46,15 @@ function Question() {
         body: '답변이당2',
         memberId: 1,
         answerer: 'kim',
+        answerStatus: 'ANSWER_ADOPTED',
         date: '2023-04-20T17:07:44.614834',
         comments: [
           {
-            commentId: 2,
-            body: '답변의 댓글이당2',
+            commentId: 1,
+            body: '답변의 댓글이당1',
             memberId: 1,
             commenter: 'kim',
-            date: '2023-04-20T17:07:51.69199',
+            date: '2023-04-19T17:07:51.69199',
           },
         ],
       },
@@ -57,21 +65,21 @@ function Question() {
         body: '질문의 댓글이당',
         memberId: 1,
         commenter: 'kim',
-        date: '2023-04-20T17:07:47.551306',
+        date: '2023-04-17T17:07:47.551306',
       },
       {
         commentId: 2,
         body: '질문의 댓글이당2',
         memberId: 1,
         commenter: 'kim',
-        date: '2023-04-20T17:07:47.551306',
+        date: '2023-04-18T17:07:47.551306',
       },
       {
         commentId: 1,
         body: '질문의 댓글이당3',
         memberId: 1,
         commenter: 'kim',
-        date: '2023-04-20T17:07:47.551306',
+        date: '2023-04-19T17:07:47.551306',
       },
     ],
   }
@@ -111,41 +119,12 @@ function Question() {
               <div className={styles.questioner}>{data.questioner}</div>
             </div>
           </div>
-          <Qcomment data={data} date={date} />
+          <Qcomment data={data} />
           <AddComment />
         </div>
       </div>
       <h1>Answer</h1>
-      <div className={styles.answer}>
-        {data.answers &&
-          data.answers.map(answer => (
-            <div className={styles.content} key={answer.answerId}>
-              <div className={styles.likes}>
-                <img
-                  src={`${process.env.PUBLIC_URL}/assets/icons/arrow-up-gray.svg`}
-                  alt='likes up button'
-                />
-                <span>좋아요</span>
-                <img
-                  src={`${process.env.PUBLIC_URL}/assets/icons/arrow-down-gray.svg`}
-                  alt='likes down button'
-                />
-              </div>
-              <div className={styles.paragraph}>
-                <p>{answer.body}</p>
-                <div className={styles.contentDown}>
-                  <Edit />
-                  <div className={styles.writer}>
-                    <div className={styles.date}>Asked {calDate(answer.date)}</div>
-                    <div className={styles.questioner}>{answer.answerer}</div>
-                  </div>
-                </div>
-                <Acomment answer={answer} date={calDate(answer.date)} />
-                <AddComment />
-              </div>
-            </div>
-          ))}
-      </div>
+      <Answer data={data} />
     </div>
   )
 }
@@ -178,7 +157,7 @@ function Edit() {
   )
 }
 
-function Qcomment({ data, date }) {
+function Qcomment({ data }) {
   return (
     <div className={styles.comment}>
       {data.comments &&
@@ -186,14 +165,14 @@ function Qcomment({ data, date }) {
           <div className={styles.body} key={comment.commentId}>
             <p>{comment.body} -</p>
             <span className={styles.commenter}> {comment.commenter}</span>
-            <span className={styles.date}> {date}</span>
+            <span className={styles.date}> {calDate(comment.date)}</span>
           </div>
         ))}
     </div>
   )
 }
 
-function Acomment({ answer, date }) {
+function Acomment({ answer }) {
   return (
     <div className={styles.comment}>
       {answer.comments &&
@@ -201,7 +180,7 @@ function Acomment({ answer, date }) {
           <div className={styles.body} key={comment.commentId}>
             <p>{comment.body} -</p>
             <span className={styles.commenter}> {comment.commenter}</span>
-            <span className={styles.date}> {date}</span>
+            <span className={styles.date}> {calDate(comment.date)}</span>
           </div>
         ))}
     </div>
@@ -219,6 +198,48 @@ function AddComment() {
         Submit
       </button>
     </form>
+  )
+}
+
+function Answer({ data }) {
+  return (
+    <div className={styles.answer}>
+      {data.answers &&
+        data.answers.map(answer => (
+          <div className={styles.content} key={answer.answerId}>
+            <div className={styles.likes}>
+              <img
+                src={`${process.env.PUBLIC_URL}/assets/icons/arrow-up-gray.svg`}
+                alt='likes up button'
+              />
+              <span>좋아요</span>
+              <img
+                src={`${process.env.PUBLIC_URL}/assets/icons/arrow-down-gray.svg`}
+                alt='likes down button'
+              />
+              {answer.answerStatus === 'ANSWER_ADOPTED' ? (
+                <img
+                  className={styles.checked}
+                  src={`${process.env.PUBLIC_URL}/assets/icons/check-green.svg`}
+                  alt='adopted answer'
+                />
+              ) : null}
+            </div>
+            <div className={styles.paragraph}>
+              <p>{answer.body}</p>
+              <div className={styles.contentDown}>
+                <Edit />
+                <div className={styles.writer}>
+                  <div className={styles.date}>Asked {calDate(answer.date)}</div>
+                  <div className={styles.questioner}>{answer.answerer}</div>
+                </div>
+              </div>
+              <Acomment answer={answer} />
+              <AddComment />
+            </div>
+          </div>
+        ))}
+    </div>
   )
 }
 
