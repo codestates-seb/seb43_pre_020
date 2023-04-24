ce1ae208d586b12712c5ef052326fd258ea6e2aa
Sangbeom Heo <mansaout@gmail.com>
2023-04-24 12:53:41 +0900

    feat: login api 함수 구현 (#91)

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,6 +1,7 @@
+import { saveRefreshTokenToLocalStorage } from '../utils/refreshTokenHandler'
 import axios from './instance'
 
-const signup = async ({ displayName, email, password }) => {
+export const signup = async ({ displayName, email, password }) => {
   try {
     await axios.post('/members', { displayName, email, password })
     return 'success'
@@ -10,4 +11,14 @@ const signup = async ({ displayName, email, password }) => {
   }
 }
 
-export default signup
+export const login = async ({ username, password, autoLogin = true }) => {
+  try {
+    const { headers } = await axios.post('/auth/login', { username, password, autoLogin })
+    axios.defaults.headers.common.Authorization = `${headers.get('authorization')}`
+    saveRefreshTokenToLocalStorage(headers.get('refresh'))
+    return 'success'
+  } catch (error) {
+    console.log(error)
+    return 'fail'
+  }
+}
