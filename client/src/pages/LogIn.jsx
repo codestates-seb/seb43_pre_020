e5f22a610a4fe15755c7467cb116f5fbb454f567
Sangbeom Heo <mansaout@gmail.com>
2023-04-24 13:13:49 +0900

    feat: 로그인 기능 구현 (#91)

--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -1,6 +1,33 @@
+import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import useRouter from '../hooks/useRouter'
 import styles from './LogIn.module.scss'
+import { login, getCurrentUserInfo } from '../api/auth'
+import { LOGIN } from '../store/authSlice'
 
 function LogIn() {
+  const { routeTo } = useRouter()
+  const dispatch = useDispatch()
+
+  const logInSubmitHandler = async event => {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+
+    const username = formData.get('email')
+    const password = formData.get('password')
+
+    const loginRes = await login({ username, password })
+
+    if (loginRes === 'success') {
+      const userInfoRes = await getCurrentUserInfo()
+      dispatch(LOGIN(userInfoRes))
+      routeTo('/')
+      return
+    }
+    alert('Sorry, you failed to log in.')
+  }
+
   return (
     <div className={styles.container}>
       <a href='/'>
@@ -20,25 +47,21 @@ function LogIn() {
           Log in with GitHub
         </button>
       </div>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={logInSubmitHandler}>
         <label className={styles.label}>
           Email
-          <input className={styles.input} type='email' required />
+          <input className={styles.input} type='email' name='email' placeholder='' required />
         </label>
         <label className={styles.label}>
           Password
-          <input className={styles.input} type='password' required />
+          <input className={styles.input} type='password' name='password' required />
         </label>
-        <label className={styles.remember}>
-          <input type='checkbox' />
-          <span> Remember me</span>
-        </label>
-        <button className={styles.LoginBtn} type='button'>
+        <button className={styles.LoginBtn} type='submit'>
           Log in
         </button>
       </form>
       <span>
-        Don’t have an account?<a href='/signup'>Sign up</a>
+        Don’t have an account?<Link to='/signup'>Sign up</Link>
       </span>
     </div>
   )
