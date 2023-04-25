b7c375de28c8ae5760e50264990d234e52d21fc8
Sangbeom Heo <mansaout@gmail.com>
2023-04-25 16:45:00 +0900

    refactor: 컴포넌트 분리 (#115)

--- client/src/pages/Question.jsx
+++ client/src/pages/Question.jsx
@@ -1,18 +1,61 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import MDEditor from '@uiw/react-md-editor'
+import useRouter from '../hooks/useRouter'
 import { getDetails, postComment, postAnswerComment } from '../api/question'
 import styles from './Question.module.scss'
 import calDate from '../utils/calDate'
-import AnswerForm from '../components/AnswerForm'
+// import AnswerForm from '../components/AnswerForm'
 
 function Question() {
+  const { id } = useParams()
+  const { isLogin, userInfo } = useSelector(state => state.auth)
   const [data, setData] = useState({})
   const [send, setSend] = useState(0)
-  const date = calDate(data.date)
-  const { id } = useParams()
-  const { userInfo } = useSelector(state => state.auth)
+
+  console.log(data.questionId)
+  console.log(data.memberId)
+
+  const handleUpClick = () => {
+    if (!isLogin) {
+      alert('로그인필요!')
+      return
+    }
+    console.log('좋아요up')
+  }
+
+  const handleDownClick = () => {
+    if (!isLogin) {
+      alert('로그인필요!')
+      return
+    }
+    console.log('좋아요down')
+  }
+
+  const handleEditClick = () => {
+    if (!isLogin) {
+      alert('로그인필요!')
+      return
+    }
+    console.log('EditClick')
+  }
+
+  const handleDeleteClick = () => {
+    if (!isLogin) {
+      alert('로그인필요!')
+      return
+    }
+    console.log('DeleteClick')
+  }
+
+  const handleSubmitComment = () => {
+    if (!isLogin) {
+      alert('로그인필요!')
+      return
+    }
+    console.log('댓글등록')
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -24,129 +67,189 @@ function Question() {
 
   return (
     <div className={styles.container}>
-      <Title data={data} date={date} />
+      <QuestionTitle title={data.title} date={data.date} view={data.view} />
       <div className={styles.content}>
-        <div className={styles.likes}>
-          <img
-            src={`${process.env.PUBLIC_URL}/assets/icons/arrow-up-gray.svg`}
-            alt='likes up button'
-          />
-          <span>{data.votes}</span>
-          <img
-            src={`${process.env.PUBLIC_URL}/assets/icons/arrow-down-gray.svg`}
-            alt='likes down button'
-          />
-        </div>
+        <Votes
+          status={null}
+          votes={data.votes}
+          upHandler={handleUpClick}
+          downHandler={handleDownClick}
+        />
         <div className={styles.paragraph} data-color-mode='light'>
           <MDEditor.Markdown source={data.content} />
           <div className={styles.contentDown}>
-            <Edit />
-            <div className={styles.writer}>
-              <div className={styles.date}>Asked {date}</div>
-              <div className={styles.questioner}>{data.questioner}</div>
-            </div>
+            <ContentController
+              writerId={data.memberId}
+              editHandler={handleEditClick}
+              deleteHandler={handleDeleteClick}
+            />
+            <Writer
+              type='question'
+              date={data.date}
+              writer={data.questioner}
+              writerId={data.memberId}
+            />
           </div>
-          <Qcomment data={data} />
-          <AddComment id={id} memberId={userInfo.memberId} setSend={setSend} />
+          <CommentList comments={data.comments} />
+          <CommentForm submitHandler={handleSubmitComment} />
         </div>
       </div>
-      <Answer data={data} id={id} userInfo={userInfo} send={send} setSend={setSend} />
-      <AnswerForm id={id} memberId={userInfo.memberId} send={send} setSend={setSend} />
+      {data.answers?.length ? <Answers data={data.answers} /> : null}
+      {/* <AnswerForm id={id} memberId={userInfo.memberId} send={send} setSend={setSend} /> */}
     </div>
   )
 }
 
-function Title({ data, date }) {
+function Answers({ data }) {
+  return (
+    <div className={styles.answer}>
+      <h1>{data.length} Answers</h1>
+      {data.map(({ answerId, answerStatus, votes, answerer, body, comments, date, memberId }) => (
+        <div className={styles.content} key={answerId}>
+          <Votes
+            status={answerStatus}
+            votes={votes}
+            upHandler={() => console.log('좋아요up')}
+            downHandler={() => console.log('좋아요down')}
+          />
+          <div className={styles.paragraph} data-color-mode='light'>
+            <MDEditor.Markdown source={body} />
+            <div className={styles.contentDown}>
+              <ContentController
+                writerId={memberId}
+                editHandler={() => console.log('click edit')}
+                deleteHandler={() => console.log('click delete')}
+              />
+              <Writer type='answer' date={date} writer={answerer} />
+            </div>
+            <CommentList comments={comments} />
+            <CommentForm submitHandler={() => console.log('댓글등록')} />
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+function ContentController({ writerId, editHandler, deleteHandler }) {
+  const { userInfo } = useSelector(state => state.auth)
+  const isShow = userInfo?.memberId === writerId
+  return (
+    isShow && (
+      <div className={styles.edit}>
+        <button type='button' onClick={editHandler}>
+          Edit
+        </button>{' '}
+        <button type='button' onClick={deleteHandler}>
+          Delete
+        </button>
+      </div>
+    )
+  )
+}
+
+function Writer({ type, date, writer, writerId }) {
+  const label = type === 'question' ? 'Asked' : 'Answered'
+
+  return (
+    <div className={styles.writer}>
+      <div className={styles.date}>
+        {label} {calDate(date)}
+      </div>
+      <div className={styles.questioner}>
+        <Link to={`/members/${writerId}`}>{writer}</Link>
+      </div>
+    </div>
+  )
+}
+
+function Votes({ status, votes, upHandler, downHandler }) {
+  return (
+    <div className={styles.likes}>
+      <button type='button' onClick={upHandler}>
+        <img
+          src={`${process.env.PUBLIC_URL}/assets/icons/arrow-up-gray.svg`}
+          alt='likes up button'
+        />
+      </button>
+      <span>{votes}</span>
+      <button type='button' onClick={downHandler}>
+        <img
+          src={`${process.env.PUBLIC_URL}/assets/icons/arrow-down-gray.svg`}
+          alt='likes down button'
+        />
+      </button>
+      {status === 'ANSWER_ADOPTED' && (
+        <img
+          className={styles.checked}
+          src={`${process.env.PUBLIC_URL}/assets/icons/check-green.svg`}
+          alt='adopted answer'
+        />
+      )}
+    </div>
+  )
+}
+
+function QuestionTitle({ title, date, view }) {
+  const { isLogin } = useSelector(state => state.auth)
+  const { routeTo } = useRouter()
+
+  const handleClick = () => {
+    if (!isLogin) {
+      alert('로그인필요!')
+    } else {
+      routeTo('/ask')
+    }
+  }
+
   return (
     <div className={styles.title}>
       <div>
-        <h1>{data.title}</h1>
-        <Link to='/ask' className={styles.btn}>
+        <h1>{title}</h1>
+        <button className={styles.btn} type='button' onClick={handleClick}>
           Ask Question
-        </Link>
+        </button>
       </div>
       <div className={styles.underTitle}>
-        Asked <span>{date}</span>
+        Asked <span>{calDate(date)}</span>
+        Viewes <span>{view}</span>
       </div>
     </div>
   )
 }
 
-function Edit() {
-  return (
-    <div className={styles.edit}>
-      <span>Edit</span>
-      <span>Delete</span>
-    </div>
-  )
-}
-
-function Qcomment({ data }) {
+function CommentList({ comments }) {
   return (
     <div className={styles.comment}>
-      {data.comments &&
-        data.comments.map(comment => (
-          <div className={styles.body} key={comment.commentId}>
-            <p>{comment.body} -</p>
-            <span className={styles.commenter}> {comment.commenter}</span>
-            <span className={styles.date}> {calDate(comment.date)}</span>
-          </div>
-        ))}
+      {comments?.map(({ commentId, body, commenter, date }) => (
+        <div className={styles.body} key={commentId}>
+          <p>{body} -</p>
+          <span className={styles.commenter}> {commenter}</span>
+          <span className={styles.date}> {calDate(date)}</span>
+        </div>
+      ))}
     </div>
   )
 }
 
-function Acomment({ answer }) {
-  return (
-    <div className={styles.comment}>
-      {answer.comments &&
-        answer.comments.map(comment => (
-          <div className={styles.body} key={comment.commentId}>
-            <p>{comment.body} -</p>
-            <span className={styles.commenter}> {comment.commenter}</span>
-            <span className={styles.date}> {calDate(comment.date)}</span>
-          </div>
-        ))}
-    </div>
-  )
-}
+function CommentForm({ submitHandler }) {
+  const formRef = useRef(null)
 
-function AddComment({ id, memberId, setSend, answerId }) {
-  const [body, setBody] = useState('')
-  const handleCommentChange = e => {
-    setBody(e.target.value)
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    const formdata = new FormData(event.currentTarget)
+    const comment = formdata.get('comment')
+    submitHandler(comment)
+    formRef.current.reset()
   }
-  const handleSubmit = e => {
-    e.preventDefault()
-    if (answerId === undefined) {
-      postComment({ id, body, memberId }).then(res => {
-        if (res === 'success') {
-          setSend(prev => prev + 1)
-          setBody('')
-        }
-      })
-    } else {
-      postAnswerComment({ id, body, memberId, answerId }).then(res => {
-        if (res === 'success') {
-          setSend(prev => prev + 1)
-          setBody('')
-        }
-      })
-    }
-  }
+
   return (
-    <form className={styles.addComment} onSubmit={handleSubmit}>
+    <form ref={formRef} className={styles.addComment} onSubmit={handleSubmit}>
       <label className={styles.label} htmlFor='comment'>
         Add comment
       </label>
-      <textarea
-        className={styles.input}
-        id='comment'
-        name='comment'
-        value={body}
-        onChange={handleCommentChange}
-        required
-      />
+      <textarea className={styles.input} id='comment' name='comment' required />
       <button className={styles.btn} type='submit'>
         Submit
       </button>
@@ -154,53 +257,47 @@ function AddComment({ id, memberId, setSend, answerId }) {
   )
 }
 
-function Answer({ data, id, userInfo, send, setSend }) {
-  return (
-    <div className={styles.answer}>
-      <h1>{data.answers && data.answers.length} Answers</h1>
-      {data.answers &&
-        data.answers.map(answer => (
-          <div className={styles.content} key={answer.answerId}>
-            <div className={styles.likes}>
-              <img
-                src={`${process.env.PUBLIC_URL}/assets/icons/arrow-up-gray.svg`}
-                alt='likes up button'
-              />
-              <span>좋아요</span>
-              <img
-                src={`${process.env.PUBLIC_URL}/assets/icons/arrow-down-gray.svg`}
-                alt='likes down button'
-              />
-              {answer.answerStatus === 'ANSWER_ADOPTED' ? (
-                <img
-                  className={styles.checked}
-                  src={`${process.env.PUBLIC_URL}/assets/icons/check-green.svg`}
-                  alt='adopted answer'
-                />
-              ) : null}
-            </div>
-            <div className={styles.paragraph} data-color-mode='light'>
-              <MDEditor.Markdown source={answer.body} />
-              <div className={styles.contentDown}>
-                <Edit />
-                <div className={styles.writer}>
-                  <div className={styles.date}>Asked {calDate(answer.date)}</div>
-                  <div className={styles.questioner}>{answer.answerer}</div>
-                </div>
-              </div>
-              <Acomment answer={answer} />
-              <AddComment
-                id={id}
-                memberId={userInfo.memberId}
-                send={send}
-                setSend={setSend}
-                answerId={answer.answerId}
-              />
-            </div>
-          </div>
-        ))}
-    </div>
-  )
-}
+// function AddComment({ id, memberId, setSend, answerId }) {
+//   const [body, setBody] = useState('')
+//   const handleCommentChange = e => {
+//     setBody(e.target.value)
+//   }
+//   const handleSubmit = e => {
+//     e.preventDefault()
+//     if (answerId === undefined) {
+//       postComment({ id, body, memberId }).then(res => {
+//         if (res === 'success') {
+//           setSend(prev => prev + 1)
+//           setBody('')
+//         }
+//       })
+//     } else {
+//       postAnswerComment({ id, body, memberId, answerId }).then(res => {
+//         if (res === 'success') {
+//           setSend(prev => prev + 1)
+//           setBody('')
+//         }
+//       })
+//     }
+//   }
+//   return (
+//     <form className={styles.addComment} onSubmit={handleSubmit}>
+//       <label className={styles.label} htmlFor='comment'>
+//         Add comment
+//       </label>
+//       <textarea
+//         className={styles.input}
+//         id='comment'
+//         name='comment'
+//         value={body}
+//         onChange={handleCommentChange}
+//         required
+//       />
+//       <button className={styles.btn} type='submit'>
+//         Submit
+//       </button>
+//     </form>
+//   )
+// }
 
 export default Question
