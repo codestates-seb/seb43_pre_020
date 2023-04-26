b93f2628df8c11b5d2a446698953176b1d6af972
Choi Yeseul <117844745+dd-stack@users.noreply.github.com>
2023-04-27 01:43:21 +0900

    [client] aside footer 내용 넣기 (#145)
    * design: aside 내용 채우기
    
    * feat: footer 내용 넣는 중
    
    * design: 푸터 내용 채우기
    
    * design: 가이드 제거

--- client/src/layout/Aside.jsx
+++ client/src/layout/Aside.jsx
@@ -1,5 +1,26 @@
 import styles from './Aside.module.scss'
 
 export default function Aside() {
-  return <aside className={styles.aside}>aside</aside>
+  return (
+    <aside className={styles.aside}>
+      <div>
+        <div className={styles.title}>The OverFlow Blog</div>
+        <ul className={styles.contents}>
+          <li>When setting up monitoring, less data is better (Ep. 563)</li>
+          <li>Instantly verify your customers online with Open Banking APIs</li>
+        </ul>
+      </div>
+      <div>
+        <div className={styles.title}>Featured on Meta</div>
+        <ul className={styles.contents}>
+          <li>Improving the copy in the close modal and post notices - 2023 edition</li>
+          <li>New blog post from our CEO Prashanth: Community is the future of AI</li>
+          <li>Temporary policy: ChatGPT is banned</li>
+          <li>The [protection] tag is being burninated</li>
+          <li>Content Discovery initiative April 13 update: Related questions using a...</li>
+          <li>Review our technical responses for the 2023 Developer Survey</li>
+        </ul>
+      </div>
+    </aside>
+  )
 }
--- client/src/layout/Footer.jsx
+++ client/src/layout/Footer.jsx
@@ -3,7 +3,66 @@ import styles from './Footer.module.scss'
 export default function Footer() {
   return (
     <footer className={styles.footerOuter}>
-      <div className={styles.footerInner}>footer</div>
+      <div className={styles.footerInner}>
+        <div className={styles.imgSort}>
+          {' '}
+          <a href='/'>
+            <img
+              className={styles.logoImg}
+              src={`${process.env.PUBLIC_URL}/assets/logo-only-graphic.svg`}
+              alt='stackoverflow logo img'
+            />
+          </a>
+          <div className={styles.box}>
+            <h5>STACK OVERFLOW</h5>
+            <ul>
+              <li>Questions</li>
+              <li>Help</li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.box}>
+          <h5>PRODUCTS</h5>
+          <ul>
+            <li>Teams</li>
+            <li>Advertising</li>
+            <li>Collectives</li>
+            <li>Talent</li>
+          </ul>
+        </div>
+        <div className={styles.box}>
+          <h5>COMPANY</h5>
+          <ul>
+            <li>About</li>
+            <li>Press</li>
+            <li>Work Here</li>
+            <li>Legal</li>
+            <li>Privacy Policy</li>
+            <li>Terms of Service</li>
+            <li>Contact Us</li>
+            <li>Cookie Settings</li>
+            <li>Cookie Policy</li>
+          </ul>
+        </div>
+        <div className={styles.box}>
+          <h5>STACK EXCHANGE NETWORK</h5>
+          <ul>
+            <li>Technology</li>
+            <li>Culture & recreation</li>
+            <li>Life & arts</li>
+            <li>Science</li>
+            <li>Professional</li>
+            <li>Business</li>
+          </ul>
+        </div>
+        <div className={styles.box2}>
+          <div>Blog</div>
+          <div>Facebook</div>
+          <div>Twitter</div>
+          <div>LinkedIn</div>
+          <div>Instagram</div>
+        </div>
+      </div>
     </footer>
   )
 }
