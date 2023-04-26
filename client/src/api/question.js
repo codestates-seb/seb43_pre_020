940f3a025f9914dc5a2c410b55908d5412937005
Sangbeom Heo <mansaout@gmail.com>
2023-04-27 00:57:43 +0900

    feat: question apis 추가 (#115)

--- client/src/api/question.js
+++ client/src/api/question.js
@@ -19,13 +19,44 @@ export async function getDetails(id) {
   }
 }
 
-export async function postComment({ id, body, memberId }) {
+export async function deleteQuestion(questionId) {
+  try {
+    await axios.delete(`/questions/${questionId}`)
+    return 'success'
+  } catch (error) {
+    console.log(error)
+    return 'fail'
+  }
+}
+
+export async function postComment(id, body, memberId) {
   try {
     const data = {
       body,
       memberId,
     }
-    await axios.post(`/questions/${id}/comments`, data)
+    const res = await axios.post(`/questions/${id}/comments`, data)
+    return res.data
+  } catch (error) {
+    console.log(error)
+    return 'fail'
+  }
+}
+
+export async function deleteComment(id, commentId) {
+  try {
+    // console.log('deleteComment의 id body commentId ', id, commentId)
+    await axios.delete(`/questions/${id}/comments/${commentId}`)
+    return 'success'
+  } catch (error) {
+    console.log(error)
+    return 'fail'
+  }
+}
+
+export async function deleteAnswerComment(questionId, answerId, commentId) {
+  try {
+    await axios.delete(`/questions/${questionId}/answers/${answerId}/comments/${commentId}`)
     return 'success'
   } catch (error) {
     console.log(error)
@@ -47,14 +78,92 @@ export async function postAnswer({ id, body, memberId }) {
   }
 }
 
-export async function postAnswerComment({ id, body, memberId, answerId }) {
+export async function patchComment(id, body, commentId) {
+  try {
+    const { data } = await axios.patch(`/questions/${id}/comments/${commentId}`, { body })
+    return data
+  } catch (error) {
+    console.log(error)
+    return 'fail'
+  }
+}
+
+export async function patchAnswerComment(questionId, answerId, commentId, body) {
+  try {
+    const { data } = await axios.patch(
+      `/questions/${questionId}/answers/${answerId}/comments/${commentId}`,
+      { body }
+    )
+    return data
+  } catch (error) {
+    console.log(error)
+    return 'fail'
+  }
+}
+
+export async function postAnswerComment(id, body, memberId, answerId) {
   try {
     const data = {
       body,
       memberId,
     }
-    await axios.post(`/questions/${id}/answers/${answerId}/comments`, data)
+    const res = await axios.post(`/questions/${id}/answers/${answerId}/comments`, data)
+    return res.data
+  } catch (error) {
+    console.log(error)
+    return 'fail'
+  }
+}
+
+export async function adoptAnswer(questionId, answerId) {
+  try {
+    const data = { answerId }
+    await axios.post(`/questions/${questionId}`, data)
     return 'success'
+  } catch (error) {
+    console.log(error)
+    return 'fail'
+  }
+}
+
+export async function voteQuestion(voteStatus, questionId, memberId) {
+  try {
+    const data = { memberId, voteStatus }
+    const res = await axios.post(`/questions/${questionId}/votes`, data)
+    return {
+      status: 'success',
+      votes: res.data,
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      status: 'fail',
+      votes: null,
+    }
+  }
+}
+
+export async function voteAnswer(voteStatus, questionId, answerId, memberId) {
+  try {
+    const data = { memberId, voteStatus }
+    const res = await axios.post(`/questions/${questionId}/answers/${answerId}/votes`, data)
+    return {
+      status: 'success',
+      votes: res.data,
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      status: 'fail',
+      votes: null,
+    }
+  }
+}
+
+export async function patchQuestion(questionId, body) {
+  try {
+    const { data } = await axios.patch(`/questions/${questionId}`, body)
+    return data
   } catch (error) {
     console.log(error)
     return 'fail'
