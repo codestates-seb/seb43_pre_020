b5de773daa6c7e8f00583e30655ea6475580d208
Sangbeom Heo <mansaout@gmail.com>
2023-04-28 04:52:34 +0900

    chore: api url 환경변수 설정 (#160)

--- client/src/api/instance.js
+++ client/src/api/instance.js
@@ -1,21 +1,19 @@
 import axios from 'axios'
 
 const request = axios.create({
-  // baseURL: '',
+  baseURL: `${process.env.REACT_APP_API_END_POINT}`,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
-    'ngrok-skip-browser-warning': '69420',
   },
 })
 
 export default request
 
 export const fileAxios = axios.create({
-  // baseURL: '',
+  baseURL: `${process.env.REACT_APP_API_END_POINT}`,
   timeout: 1000,
   headers: {
     'Content-Type': 'multipart/form-data',
-    'ngrok-skip-browser-warning': '69420',
   },
 })
